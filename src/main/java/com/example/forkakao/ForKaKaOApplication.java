b7c3d7561aaa3200d7e1b6f3b115886ef00e01d4
kudongku <kudongku@naver.com>
2024-04-05 22:07:23 +0900

    :tada: feat(git) : git init commit

---
+++ src/main/java/com/example/forkakao/ForKaKaOApplication.java
@@ -0,0 +1,13 @@
+package com.example.forkakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForKaKaOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForKaKaOApplication.class, args);
+    }
+
+}
