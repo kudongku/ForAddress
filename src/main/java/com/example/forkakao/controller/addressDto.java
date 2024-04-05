7f95134c1657a4e78ac3a44a3611a7eed52e5c52
kudongku <kudongku@naver.com>
2024-04-05 23:29:12 +0900

    :sparkles: feat(address) : DTO 생성

---
+++ src/main/java/com/example/forkakao/controller/addressDto.java
@@ -0,0 +1,10 @@
+package com.example.forkakao.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class addressDto {
+    private String address;
+}
